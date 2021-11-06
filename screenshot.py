0387170fe7971b1e871e4625e69605d56852d8e1
sibainu2 <84668924+sibainu2@users.noreply.github.com>
2021-11-06 16:11:14 +0900

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,10 +2,10 @@ import pyautogui
 
 from time import sleep
 sleep(3)
-
+#このファイルではスクリーンショットを取ります
 def screenshot(sx,sy):
     #スクリーンを撮る
-    screenshot1 = pyautogui.screenshot(region = (100, 330, 1100, 830))#9x8
+    screenshot1 = pyautogui.screenshot(region = (100, 330, 1100, 830))#座標指定してスクリーンショット
     screenshot1.save(r"C:\Users\waon-pc\Desktop\pythonC\discord\lordsmobileAPI\dete\Photo\map_"+str(sx)+"_"+str(sy)+".png")#保存先
 #    screenshot2 = pyautogui.screenshot(region = (105, 240, 1080, 778))#1170
 #    screenshot2.save(r"C:\Users\waon-pc\Desktop\pythonCドラ\discord\lordsmobileAPI\dete\Photo\map_"+str(i)+"_2.png")#バリア用
